df3dddd1965b9d49d30d8b476dda4b8a80e2edba
Nik0-9 <filannino.nicola9@gmail.com>
2024-04-17 19:29:20 +0200

    ADD flip class in CardComponent

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,17 +1,72 @@
 <template>
-    <div>
-        <img :src="image" :alt="title">
+    <div class=" flip-card">
+        <div class="flip-card-inner">
+            <div class="flip-card-front">
+                <img :src="image" :alt="title">
+            </div>
+            <div class="flip-card-back">
+                <div>{{ title }}</div>
+                <div>{{ originalTitle }}</div>
+                <div>{{ vote }}</div>
+                <div>{{ language }}</div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'CardComponent',
-        props:[ 'title', 'originalTitle', 'language', 'vote', 'image']
-    }
+export default {
+    name: 'CardComponent',
+    props: ['title', 'originalTitle', 'language', 'vote', 'image']
+}
 </script>
 
 <style lang="scss" scoped>
+@use '../assets/styles/partials/variables' as *;
 
+.flip-card {
+    background-color: transparent;
+    width: 344px;
+    height: 515px;
+    perspective: 1000px;
+}
 
+.flip-card-inner {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    text-align: center;
+    transition: transform 0.8s;
+    transform-style: preserve-3d;
+}
+
+.flip-card:hover .flip-card-inner {
+    transform: rotateY(180deg);
+}
+
+.flip-card-front,
+.flip-card-back {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+}
+
+.flip-card-front {
+    background-color: transparent;
+}
+
+.flip-card-back {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 100%;
+    background-color: $navbarcolor;
+    color: white;
+    transform: rotateY(180deg);
+    div{
+        margin: 10px 0;
+    }
+}
 </style>
--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,33 +1,36 @@
 <template>
-    <div class="row m-0">
+    <div class="row">
         <h2 v-if="store.qString.params.query">film</h2>
-        <div class="col " v-for="item in store.movies" >
-            <CardComponent :image="store.baseUrlImg + item.poster_path"/>
+        <div class="col " v-for="item in store.movies">
+            <CardComponent :image="store.baseUrlImg + item.poster_path" :title="item.title"
+                :originalTitle="item.original_title" :language="item.original_language" :vote="item.vote_average" />
         </div>
         <h2 v-if="store.qString.params.query">serie tv</h2>
         <div class="col " v-for="item in store.series">
-            <CardComponent :image="store.baseUrlImg + item.poster_path"/>
+            <CardComponent :image="store.baseUrlImg + item.poster_path" :title="item.name"
+                :originalTitle="item.original_name" :language="item.original_language" :vote="item.vote_average" />
         </div>
     </div>
 </template>
 
 <script>
-import {store} from '../store';
+import { store } from '../store';
 import CardComponent from './CardComponent.vue';
-    export default {
-        name: 'MovieList',
-        components:{CardComponent},
-        data(){
-            return{
-                store
-            }
+export default {
+    name: 'MovieList',
+    components: { CardComponent },
+    data() {
+        return {
+            store,
         }
     }
+}
 </script>
 
 <style lang="scss" scoped>
 @use '../assets/styles/partials/variables' as *;
-h2{
+
+h2 {
     text-transform: uppercase;
     color: $titlecolor;
     margin: 20px 0;
